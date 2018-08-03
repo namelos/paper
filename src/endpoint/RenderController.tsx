882d22a90f77dbcfaefce791aaa3cd4ec7394d34
namelos <namelos@outlook.com>
2018-08-03 23:34:43 +0800

    add context to ssr

--- src/endpoint/RenderController.tsx
+++ src/endpoint/RenderController.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import { Controller, Get, Req } from 'routing-controllers'
-import { renderToStaticMarkup, renderToString } from 'react-dom/server'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import ApolloClient from 'apollo-client/ApolloClient'
 import { SchemaLink } from 'apollo-link-schema'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloProvider, getDataFromTree } from 'react-apollo'
+import { SchemaService } from 'bootstrap/SchemaService'
 import { App } from 'common/App'
 import { Html } from 'common/components/Html'
-import { SchemaService } from 'bootstrap/SchemaService'
+import { Request, Response } from 'express'
+import React from 'react'
+import { ApolloProvider, getDataFromTree } from 'react-apollo'
+import { renderToStaticMarkup, renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
-import { Request } from 'express'
+import { Controller, Get, Req, Res } from 'routing-controllers'
 
 @Controller()
 export class RenderController {
   constructor(private schemaService: SchemaService) {}
 
   @Get('*')
-  async get(@Req() request: Request) {
+  async get(@Req() req: Request, @Res() res: Response) {
     const client = new ApolloClient({
       ssrMode: true,
-      link: new SchemaLink({ schema: this.schemaService.schema }),
+      link: new SchemaLink({
+        schema: this.schemaService.schema,
+        context: { req, res }
+      }),
       cache: new InMemoryCache()
     })
 
     const context = {}
 
     const Root = <ApolloProvider client={client}>
-      <StaticRouter location={request.url} context={context}>
+      <StaticRouter location={req.url} context={context}>
         <App />
       </StaticRouter>
     </ApolloProvider>
